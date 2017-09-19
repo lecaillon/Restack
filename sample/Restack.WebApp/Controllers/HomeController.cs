66aea59acb2f6b58389e46dab0e67c5c0a3e6793
Philippe Lécaillon <lecaillon@hotmail.com>
2017-09-19 15:04:34 +0200

    Add a sample web project

---
+++ sample/Restack.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Restack.Refit;
+using Restack.WebApp.Models;
+using Restack.WebApp.Services;
+
+namespace Restack.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IGeoApi _geoApi;
+
+        public HomeController(IRestClient<IGeoApi> geoApiClient)
+        {
+            _geoApi = geoApiClient.Client;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> About()
+        {
+            var regions = await _geoApi.GetRegionsAsync();
+            ViewData["Message"] = string.Join(", ", regions.Select(x => x.Nom));
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ sample/Restack.WebApp/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Restack.WebApp.Services;
+
+namespace Restack.WebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddRestack();
+
+            services.AddRestackGlobalHeaders(o => o.Headers.Add("user-agent", "myagent"));
+
+            services.AddRestClient<IGeoApi>("https://geo.api.gouv.fr")
+                    .AddRestackHeaders<IGeoApi>(o => o.Headers.Add("api-key", "xxxxx-xxx-xxxxxxxx"));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
